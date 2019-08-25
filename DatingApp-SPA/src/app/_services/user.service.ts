7347d873f7bc2b6ec82f321942c41636b58798b6
haristauqir <haristauqir@gmail.com>
2019-08-25 12:37:08 +0100

    delete profile photo

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -35,5 +35,9 @@ constructor(private http: HttpClient) { }
     return this.http.post<User>(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain', {});
   }
 
+  deletePhoto(userId: number, id: number) {
+    return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
+  }
+
 
 }
