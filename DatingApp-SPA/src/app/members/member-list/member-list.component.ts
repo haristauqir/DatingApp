6a3a26aac0d5f943d2064264f4732ec1666e6b59
haristauqir <haristauqir@gmail.com>
2019-09-04 12:45:52 +0100

    filtering in ASP.net CORE

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -3,7 +3,7 @@ import { User } from 'src/app/_models/user';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
-import { Pagination } from 'src/app/_models/pagination';
+import { Pagination, PaginatedResult } from 'src/app/_models/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -20,15 +20,27 @@ export class MemberListComponent implements OnInit {
       this.users = data['users'].result;
       this.pagination = data['users'].pagination;
     });
-    /*this.loadUsers(); */
   }
 
-  /* loadUsers() {
-    this.userService.getUsers().subscribe((users: User[]) => {
-      this.users = users;
-    }, error => {
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+   loadUsers() {
+    this.userService.
+    getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+    .subscribe(
+      (res: PaginatedResult<User[]>) => {
+      this.users = res.result;
+      this.pagination = res.pagination;
+    },
+    error => {
       this.alertify.error(error);
-    });
-  } */
+    }
+    );
+  }
+
 
 }
